c3d4aad568f35e0be8c95432b6407544222c9511
NizarKratos22 <hajounimottaki@zaghouan.r-iset.tn>
2021-08-14 22:56:46 +0100

    HamemaVoyage

---
+++ assets/config.js
@@ -0,0 +1,39 @@
+// cpanel - site_templates/countdown_musician/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        email: "razouane@gmail.com",
+        logo: "Voyager Avec Nous",
+        social: [
+            
+            {
+                icon: 'facebook',
+                link: "https:\/\/www.facebook.com\/voyagezavecnous2018"
+            },
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+    },
+    slides: [
+        {
+            type: 'countdown',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "Réserver votre voyage",
+            buttonLink: "",
+            endTime: '2018-11-09T17:00:00.000Z',
+            content: "",
+        }
+    ]
+};
